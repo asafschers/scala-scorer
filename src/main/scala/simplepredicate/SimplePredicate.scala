9c4f92b3d39acd5bbb3e4d5e034a77d0a9df4027
asaf schers <schers@riskified.com>
2018-03-25 02:29:07 +0300

    SimplePredicate - refactors

--- src/main/scala/simplepredicate/SimplePredicate.scala
+++ src/main/scala/simplepredicate/SimplePredicate.scala
@@ -5,12 +5,12 @@ object SimplePredicate {
     val field: String = (xml_predicate \ "@field").text
     val operator: String = (xml_predicate \ "@operator").text
     val stringValue: String = (xml_predicate \ "@value").text
-    val value = try { Left(stringValue.toDouble) } catch { case _ => Right(stringValue) }
-    new SimplePredicate(field, operator, value)
+    val predicateValue = try { Left(stringValue.toDouble) } catch { case _ => Right(stringValue) }
+    new SimplePredicate(field, operator, predicateValue)
   }
 }
 
-class SimplePredicate(field: String, operator: String, value: Either[Double,String]) {
+class SimplePredicate(field: String, operator: String, predicateValue: Either[Double,String]) {
   val GREATER_THAN     = "greaterThan"
   val LESS_THAN        = "lessThan"
   val LESS_OR_EQUAL    = "lessOrEqual"
@@ -20,31 +20,12 @@ class SimplePredicate(field: String, operator: String, value: Either[Double,Stri
   val MATH_OPS = Array(GREATER_THAN, LESS_THAN, LESS_OR_EQUAL, GREATER_OR_EQUAL)
 
   def isTrue(features: Map[String, Either[Double, String]]): Either[Boolean, String] = {
-    val featureValue = features.get(field)
-    value match {
+    val inputValue = features.get(field)
+    predicateValue match {
       case Left(numValue) =>
-        isTrueNum(featureValue, numValue)
+        isTrueNum(inputValue, numValue)
       case Right(strValue) =>
-        isTrueStr(featureValue, strValue)
-    }
-  }
-
-  private def isTrueStr(featureValue: Option[Either[Double, String]], strValue: String) = {
-    featureValue match {
-      case Some(Left(_)) =>
-        Left(false)
-      case Some(Right(featureStrValue)) =>
-        operator match {
-          case IS_MISSING =>
-            Left(featureStrValue == "")
-          case EQUALS =>
-            Left(featureStrValue == strValue)
-        }
-      case None =>
-        operator match {
-          case IS_MISSING =>
-            Left(featureValue.isEmpty)
-        }
+        isTrueStr(inputValue, strValue)
     }
   }
 
@@ -64,7 +45,26 @@ class SimplePredicate(field: String, operator: String, value: Either[Double,Stri
             Left(featureNumValue == numValue)
         }
       case Some(Right(_)) =>
-        Right("Expected Numerical feature")
+        Left(false)
+    }
+  }
+
+  private def isTrueStr(featureValue: Option[Either[Double, String]], strValue: String) = {
+    featureValue match {
+      case Some(Left(_)) =>
+        Left(false)
+      case Some(Right(featureStrValue)) =>
+        operator match {
+          case IS_MISSING =>
+            Left(featureStrValue == "")
+          case EQUALS =>
+            Left(featureStrValue == strValue)
+        }
+      case None =>
+        operator match {
+          case IS_MISSING =>
+            Left(featureValue.isEmpty)
+        }
     }
   }
 }
