51b74defda2fc5d6ed5ff5e2d4926b354a40cdff
asaf schers <schers@riskified.com>
2018-03-25 00:03:57 +0300

    SimplePredicate value - either double or string

--- src/main/scala/simplepredicate/SimplePredicate.scala
+++ src/main/scala/simplepredicate/SimplePredicate.scala
@@ -1,6 +1,7 @@
 package simplepredicate
 
-class SimplePredicate(field: String, operator: String, value: Double) {
+
+class SimplePredicate(field: String, operator: String, value: Either[Double,String]) {
   val GREATER_THAN     = "greaterThan"
   val LESS_THAN        = "lessThan"
   val LESS_OR_EQUAL    = "lessOrEqual"
@@ -10,39 +11,43 @@ class SimplePredicate(field: String, operator: String, value: Double) {
   val MATH_OPS = Array(GREATER_THAN, LESS_THAN, LESS_OR_EQUAL, GREATER_OR_EQUAL)
 
   def isTrue(features: Map[String, Option[Double]]): Either[Boolean, String] = {
-    features.get(field).flatten match {
-      case Some(featureValue) =>
-        operator match {
-          case GREATER_THAN =>
-            Left(featureValue > value)
-          case LESS_THAN =>
-            Left(featureValue < value)
-          case GREATER_OR_EQUAL =>
-            Left(featureValue >= value)
-          case LESS_OR_EQUAL =>
-            Left(featureValue <= value)
-          case EQUALS =>
-             Left(featureValue == value)
-          case _ =>
-            Right("Unknown Operator")
+    value match {
+      case Left(numValue) =>
+        features.get(field).flatten match {
+          case Some(featureValue) =>
+            operator match {
+              case GREATER_THAN =>
+                Left(featureValue > numValue)
+              case LESS_THAN =>
+                Left(featureValue < numValue)
+              case GREATER_OR_EQUAL =>
+                Left(featureValue >= numValue)
+              case LESS_OR_EQUAL =>
+                Left(featureValue <= numValue)
+              case EQUALS =>
+                Left(featureValue == numValue)
+              case _ =>
+                Right("Unknown Operator")
+            }
+          case None =>
+            operator match {
+              case IS_MISSING =>
+                Left(features.get(field).flatten.isEmpty)
+              case _ =>
+                Right("Missing Feature")
+            }
         }
-      case None =>
-        operator match {
-          case IS_MISSING =>
-            Left(features.get(field).flatten.isEmpty)
-          case _ =>
-            Right("Missing Feature")
-        }
+//      case Right(numValue) =>
     }
   }
 }
-
 
 object SimplePredicate {
   def setFromXml(xml_predicate: scala.xml.Elem): SimplePredicate = {
     val field: String = (xml_predicate \ "@field").text
     val operator: String = (xml_predicate \ "@operator").text
-    val value: Double = (xml_predicate \ "@value").text.toFloat
+    val stringValue: String = (xml_predicate \ "@value").text
+    val value = try { Left(stringValue.toDouble) } catch { case _ => Right(stringValue) }
     new SimplePredicate(field, operator, value)
   }
 }
