c549b8e2e54d8001067a1513288049643237ad74
asaf schers <schers@riskified.com>
2018-04-06 17:51:30 +0300

    Scala node infrastructure

---
+++ src/main/scala/node/Node.scala
@@ -0,0 +1,25 @@
+package node
+
+import value._
+
+
+//object Node {
+//  def setFromXml(xml_predicate: scala.xml.Elem): Node = {
+//    // format predicate, decision and children
+//    new Node(predicate: predicate, decision: decision, chilren: children)
+//  }
+//}
+//
+//sealed trait Predicate
+//final case class SimplePredicate(b: SimplePredicate) extends Predicate
+//final case class SimpleSetPredicate(a: SimpleSetPredicate) extends Predicate
+//
+//class Node(predicate: Predicate, decision: decision, children: children) {
+//
+//  def isTrue(features: Map[String, Value]) = {
+//
+//  }
+//
+//}
+//
+
