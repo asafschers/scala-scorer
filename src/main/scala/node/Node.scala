175314910093db637fe5b9314b747b7a82012bd7
asaf schers <schers@riskified.com>
2018-04-07 01:54:07 +0300

    Add Node object and class

--- src/main/scala/node/Node.scala
+++ src/main/scala/node/Node.scala
@@ -1,25 +1,17 @@
 package node
-
+import predicate._
 import value._
 
+object Node {
+  def setFromXml(xml_predicate: scala.xml.Elem): Node = {
+    val score: Double = (xml_predicate \ "@score").text.toDouble
+    val predicate: Predicate = apply(xml_predicate)
+    new Node(predicate, score, xml_predicate.child)
+  }
+}
 
-//object Node {
-//  def setFromXml(xml_predicate: scala.xml.Elem): Node = {
-//    // format predicate, decision and children
-//    new Node(predicate: predicate, decision: decision, chilren: children)
-//  }
-//}
-//
-//sealed trait Predicate
-//final case class SimplePredicate(b: SimplePredicate) extends Predicate
-//final case class SimpleSetPredicate(a: SimpleSetPredicate) extends Predicate
-//
-//class Node(predicate: Predicate, decision: decision, children: children) {
-//
-//  def isTrue(features: Map[String, Value]) = {
-//
-//  }
-//
-//}
-//
-
+class Node(predicate: Predicate, score: Double, children: Seq[scala.xml.Node]) {
+  def isTrue(features: Map[String, Value]) = {
+    true
+  }
+}
