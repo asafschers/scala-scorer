51f32dcbfed3aa0a44b37979fb7b2bcf751ad4d1
asaf schers <schers@riskified.com>
2018-04-14 14:52:01 +0300

    Node - format predicate and children

--- src/main/scala/node/Node.scala
+++ src/main/scala/node/Node.scala
@@ -4,14 +4,16 @@ import value._
 
 object Node {
   def fromXml(xmlPredicate: scala.xml.Node): Node = {
-    val score: Double = (xmlPredicate \ "@score").text.toDouble
-    val predicate: Predicate = Predicate.apply(xmlPredicate)
-    val children: Seq[Node] = xmlPredicate.child.map(xml => fromXml(xml))
+    val score: Option[Double] = try { Some((xmlPredicate \ "@score").text.toDouble) } catch { case _ => None }
+    val predicate: Predicate = Predicate.apply(xmlPredicate.child(0))
+    val children: Seq[Node] = xmlPredicate.child.drop(1).map(xml => fromXml(xml))
     new Node(predicate, score, children)
   }
 }
 
-class Node(predicate: Predicate, score: Double, children: Seq[Node]) {
+class Node(predicate: Predicate, inputScore: Option[Double], children: Seq[Node]) {
+  val score = inputScore
+
   def isTrue(features: Map[String, Value]): Either[String, Boolean] = {
     predicate.isTrue(features)
   }
