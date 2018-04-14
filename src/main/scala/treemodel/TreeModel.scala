b9f03a624f038fd2c9be4a3bc4176884fdfe6cd5
asaf schers <schers@riskified.com>
2018-04-14 14:39:11 +0300

    TreeModel  Send first Node in xml to root

--- src/main/scala/treemodel/TreeModel.scala
+++ src/main/scala/treemodel/TreeModel.scala
@@ -6,14 +6,15 @@ import value._
 object TreeModel {
   def fromXml(xmlPredicate: scala.xml.Node): TreeModel = {
     val id: String = (xmlPredicate \ "@id").text
-    val root: Node = Node.fromXml(xmlPredicate)
+    val node = (xmlPredicate \ "Node").head
+    val root: Node = Node.fromXml(node)
     new TreeModel(id, root)
   }
 }
 
 class TreeModel(id: String, root: Node) {
 
-  def decide(features: Map[String, Value]): Double = {
+  def decide(features: Map[String, Value]): Option[Double] = {
     val current: Node = root
 
     current.score
