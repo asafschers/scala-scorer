7ef425e8e22f1810ab7531d8563e99138cc5cff3
asaf schers <schers@riskified.com>
2018-04-21 11:30:27 +0300

    Add TreeModelSpec

--- src/test/scala/treemodel/TreeModelSpec.scala
+++ src/test/scala/treemodel/TreeModelSpec.scala
@@ -15,6 +15,30 @@ class TreeModelSpec extends FlatSpec with Matchers {
   val treeModel: TreeModel = TreeModel.fromXml(trimmedTreePmml)
 
   it should "score for no features" in {
-//    treeModel.decide(Map()) shouldEqual 4
+    treeModel.decide(Map()) shouldEqual Some(4.3463944950723456E-4)
+  }
+
+  it should "f2 first true" in {
+    treeModel.decide(Map("f2" -> CategoricalValue("f2v1"))) shouldEqual Some(-1.8361380219689046E-4)
+  }
+
+  it should "f1, f2 first true" in {
+    val features = Map("f2" -> CategoricalValue("f2v1"), "f1" -> CategoricalValue("f1v3"))
+    treeModel.decide(features) shouldEqual Some(-6.237581139073701E-4)
+  }
+
+  it should "f1, f2, f4  first true" in {
+    val features = Map("f2" -> CategoricalValue("f2v1"), "f1" -> CategoricalValue("f1v3"), "f4" -> NumericalValue(0.08))
+    treeModel.decide(features) shouldEqual Some(0.0021968294712358194)
+  }
+
+  it should "f1, f2 first true, f4 second true" in {
+    val features = Map("f2" -> CategoricalValue("f2v1"), "f1" -> CategoricalValue("f1v3"), "f4" -> NumericalValue(0.09))
+    treeModel.decide(features) shouldEqual Some(-9.198573460887271E-4)
+  }
+
+  it should "f2 none are true" in {
+    val features = Map("f2" -> CategoricalValue("f2v7"))
+    treeModel.decide(features) shouldEqual None
   }
 }
