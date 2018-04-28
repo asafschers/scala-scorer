4136b30858c1c5b0838f68b9dec6c6585e743fc3
asaf schers <schers@riskified.com>
2018-04-28 13:36:10 +0300

    SimpleSetPredicate - change array to list

--- src/main/scala/simplesetpredicate/SimpleSetPredicate.scala
+++ src/main/scala/simplesetpredicate/SimpleSetPredicate.scala
@@ -8,16 +8,16 @@ object SimpleSetPredicate {
   def fromXml(xmlPredicate: scala.xml.Node): SimpleSetPredicate = {
     val field: String = (xmlPredicate \ "@field").text
     val operator: String = (xmlPredicate \ "@operator").text
-    val values: Array[String] = formatValuesArray((xmlPredicate \ "Array").text)
+    val values: List[String] = formatValues((xmlPredicate \ "Array").text)
     new SimpleSetPredicate(field, operator, values)
   }
 
-  def formatValuesArray(valuesInput: String): Array[String] = {
-    ("\"(.*?)\"|([^\\s]+)").r.findAllIn(valuesInput).toArray.map(value => value.replace("\"",""))
+  def formatValues(valuesInput: String): List[String] = {
+    ("\"(.*?)\"|([^\\s]+)").r.findAllIn(valuesInput).toList.map(value => value.replace("\"",""))
   }
 }
 
-class SimpleSetPredicate(field: String, operator: String, values: Array[String]) extends Predicate {
+class SimpleSetPredicate(field: String, operator: String, values: List[String]) extends Predicate {
   val IS_IN = "is_in"
 
   def isTrue(features: Map[String, Value]): Either[String, Boolean] = {
